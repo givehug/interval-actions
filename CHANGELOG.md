640a05ee84aef332d35e13ff015867d9a728f5fd
givehug <vboretskyi@gmail.com>
2020-04-06 19:49:24 +0200

    Fix npm audit issues, improve UTs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,18 @@
-3.0.0 / 2018-10-19
-------------------
+## 3.0.3 / 2020-04-06
+
+- fix mocha minimist security issue
+
+## 3.0.2 / 2020-04-06
+
+- fix npm audit
+- added circleci
+- improved unit tests
+
+## 3.0.0 / 2018-10-19
 
 - **BREAKING:** makeQueue and makeStack deprecated, module now exports only method `interval`, see usage example in README
 
-2.0.1 / 2018-10-16
-------------------
+## 2.0.1 / 2018-10-16
 
 - **BREAKING:** Change imports, now you have to use the following syntax `import * as actions from 'interval-actions'`
 - Add TypeScript definitions
--- src/queue.ts
+++ src/queue.ts
@@ -1,34 +1,34 @@
 import Node from './node';
 
 export default class Queue<T> {
-    private first?: Node<T> = null;
-    private last?: Node<T> = null;
-    private _size: number = 0;
+  private first?: Node<T> = null;
+  private last?: Node<T> = null;
+  private _size: number = 0;
 
-    get size() {
-        return this._size;
+  get size() {
+    return this._size;
+  }
+
+  enqueue = (val: T): void => {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
     }
-	
-    enqueue = (val: T): void => {
-        const newNode = new Node(val);
-        if (!this.first) {
-            this.first = newNode;
-            this.last = newNode;
-        } else {
-            this.last.next = newNode;
-            this.last = newNode;
-        }
-        this._size++;
-	};
+    this._size++;
+  };
 
-    dequeue = (): T | null => {
-        if (!this.first) return null;
-        const temp = this.first;
-        if (this.first === this.last) {
-            this.last = null;
-        }
-        this.first = this.first.next;
-        this._size--;
-        return temp.value;
-    };
+  dequeue = (): T | null => {
+    if (!this.first) return null;
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this._size--;
+    return temp.value;
+  };
 }
