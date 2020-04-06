a62c75cd8c6e296396cbe8d8a0ca4e1a215d1ab5
givehug <vboretskyi@gmail.com>
2020-04-06 19:49:24 +0200

    Fix npm audit issues, improve UTs

--- test/test.ts
+++ test/test.ts
@@ -1,28 +1,34 @@
 import assert from 'assert';
-import {interval} from '../src/interval';
+import { interval } from '../src/interval';
 
 describe('Test interval', () => {
-	it('should call callback functions with given interval', done => {
-		const arr = Array.from(Array(5), (_, i) => i);
-		const time = 200;
-		let timeStamp = Date.now();
+  it('should call callback functions with given interval', (done) => {
+    const arr = Array.from(Array(5), (_, i) => i);
+    const timeout = 200;
+    let timeStamp = Date.now();
+    let prevTimestamp = 0;
 
-		const action = interval((i: number) => {
-			const now = Date.now();
+    const action = interval((i: number) => {
+      const now = Date.now();
+      const timeDiff = now - timeStamp;
 
-			if (i === 0) {
-				assert.ok(now - timeStamp < time, `correct time dif ${i}`);
-			} else {
-				assert.ok(now - timeStamp > time, `correct time dif ${i}`);
-			}
+      assert.ok(prevTimestamp < now);
 
-			if (i === arr.length - 1) {
-				done();
-			}
+      prevTimestamp = now;
 
-			timeStamp = now;
-		}, time);
+      if (i === 0) {
+        assert.ok(timeDiff < timeout, `correct time dif ${i} - ${timeDiff}`);
+      } else {
+        assert.ok(timeDiff >= timeout, `correct time dif ${i} - ${timeDiff}`);
+      }
 
-		arr.forEach(action);
-	});
+      if (i === arr.length - 1) {
+        done();
+      }
+
+      timeStamp = now;
+    }, timeout);
+
+    arr.forEach(action);
+  });
 });
